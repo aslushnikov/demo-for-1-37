fc3d4709e158c94eefe7a7d417c917c52b35af7f
Andrey Lushnikov <lushnikov@chromium.org>
2023-08-03 18:08:52 +0200

    moar nits

--- a.spec.ts
+++ a.spec.ts
@@ -4,6 +4,9 @@ for (let i = 0; i < 20; ++i) {
   const url = `https://example.com/?${i}`;
   test(`should navigate to ${url}`, async ({ page }) => {
     const response = await page.goto(url);
+    // Fail the test.
+    if (process.env.GITHUB_RUN_ATTEMPT === '1' && url.includes('15'))
+      expect(true).toBe(false);
     expect(response.status()).toBe(200);
   });
 }
